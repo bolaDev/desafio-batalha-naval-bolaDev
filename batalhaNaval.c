df1daca133c63bc03010556e8deac805ea801b5d
bolaDev <60231147+bolaDev@users.noreply.github.com>
2026-03-18 15:25:17 +0000

    Criei logica para alocar navio na vertical

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,14 +6,16 @@
 #include <stdio.h>
 
 int main(void) {
+    //Vetor para coordenada alfabetica do tabuleiro
     char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    //Matriz do tabuleiro
     int tabuleiro[10][10];
 
     printf(" TABULEIRO BATALHA NAVAL\n");
     printf("  ");
 
     for (int j = 0; j < 10; j++) {
-        printf("%c ", linha[j]);
+        printf("%c ", linha[j]);  //Imprime coordenada alfabetica do tabuleiro
     }
     printf("\n");
 
@@ -27,38 +29,16 @@ int main(void) {
                 tabuleiro[i][j] = 0;      // água
             }
 
+            if(j == 8 && (i == 5 || i == 6 || i == 7 ))
+            {
+                tabuleiro[i][j] = 3;
+            }
+
             printf(" %d", tabuleiro[i][j]);
         }
         printf("\n");
     }
 
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
