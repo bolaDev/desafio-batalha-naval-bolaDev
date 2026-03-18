d556f1f44b8e822b5d248bbb925244acd24db21e
bolaDev <60231147+bolaDev@users.noreply.github.com>
2026-03-18 20:04:17 +0000

    new version of committed

---
+++ batalhNaval.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// Desafio Batalha Naval - MateCheck
+
+
+#include <stdio.h>
+
+int main(void) {
+    //Vetor para coordenada alfabetica do tabuleiro
+    char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    //Matriz do tabuleiro
+    int tabuleiro[10][10];
+
+    printf(" TABULEIRO BATALHA NAVAL\n");
+    printf("  ");
+
+    for (int j = 0; j < 10; j++) {
+        printf("%c ", linha[j]);  //Imprime coordenada alfabetica do tabuleiro
+    }
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", i + 1);
+        for (int j = 0; j < 10; j++) {
+
+            if (i == 2 && (j == 3 || j == 4 || j == 5)) {
+                tabuleiro[i][j] = 3;      // parte do navio
+            } else {
+                tabuleiro[i][j] = 0;      // água
+            }
+
+            if(j == 8 && (i == 5 || i == 6 || i == 7 ))
+            {
+                tabuleiro[i][j] = 3;
+            }
+
+            printf(" %d", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
