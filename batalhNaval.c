5cf05501c69124d5218afee8af1a37c2e7c626d3
rodrigo dos santos rosa <rodrigorosasant.com@gmail.com>
2026-03-20 12:08:02 +0000

    implementei documentação do código

--- batalhNaval.c
+++ batalhNaval.c
@@ -12,7 +12,7 @@ int main(void) {
     int tabuleiro[10][10];
 
     printf(" TABULEIRO BATALHA NAVAL\n");
-    printf("  ");
+    printf("  ");//Espaço para alinhar corretamente a coordenada alfabetica
 
     for (int j = 0; j < 10; j++) {
         printf("%c ", linha[j]);  //Imprime coordenada alfabetica do tabuleiro
@@ -20,25 +20,25 @@ int main(void) {
     printf("\n");
 
     for (int i = 0; i < 10; i++) {
-        printf("%d ", i + 1);
+        printf("%d ", i + 1);//Imprime coordenada numérica 
         for (int j = 0; j < 10; j++) {
 
             tabuleiro[i][j] = 0;      // água
         
             if (i == 2 && (j == 3 || j == 4 || j == 5)) {
-                tabuleiro[i][j] = 3;      // parte do navio
+                tabuleiro[i][j] = 3; //Navio horizontal
             } 
 
             if(j == 8 && (i == 5 || i == 6 || i == 7 ))
             {
-                tabuleiro[i][j] = 3;
+                tabuleiro[i][j] = 3; //Navio Vertical
             }
 
             if((i == 6 &&  j == 0)|| 
                (i== 7 &&j == 1)|| 
                (i == 8 && j == 2))
             {
-                tabuleiro[i][j] = 3;
+                tabuleiro[i][j] = 3; //Navio Diagonal
             }
 
             printf(" %d", tabuleiro[i][j]);
