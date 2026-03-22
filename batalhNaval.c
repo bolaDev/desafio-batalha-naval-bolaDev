9289c3d6eac2c259fc9a1caaa4c06a56b16d9b75
rodrigo dos santos rosa <rodrigorosasant.com@gmail.com>
2026-03-22 21:27:44 +0000

    implementei logica para criação de habilidades

--- batalhNaval.c
+++ batalhNaval.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
-
-
-#include <stdio.h>
+#define LINHA 10
+#define COLUNA 10
 
 int main(void) {
     //Vetor para coordenada alfabetica do tabuleiro
     char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
     //Matriz do tabuleiro
     int tabuleiro[10][10];
+    int tabuleiroHabilidades[10][10];
 
     printf(" TABULEIRO BATALHA NAVAL\n");
     printf("  ");//Espaço para alinhar corretamente a coordenada alfabetica
@@ -53,7 +53,71 @@ int main(void) {
     }
 
     printf("Tabuleiro com habilidades\n");
+    printf("\n");
+    
+    for(int i = 0;i < LINHA; i++)
+    {
+        printf(" %c",linha[i]);
+    }
 
+    printf("\n");
+
+    for(int i =0;i < LINHA;i ++)
+    {
+        printf("%d ",i);
+        for(int j = 0;j < COLUNA;j++)
+        {
+            tabuleiroHabilidades[i][j]=0; //agua
+
+            if((i == 0 && j == 3)||
+               (i == 1 && j == 3)||
+               (i == 1 && j == 3)||
+               (i == 2 && j == 3)||
+               (i == 2 && j == 1)||
+               (i == 2 && j == 2)||
+               (i == 2 && j == 3)||
+               (i == 2 && j == 4)||
+               (i == 2 && j == 5)||
+               (i == 3 && j == 3)||
+               (i == 4 && j == 3))
+               {
+                tabuleiroHabilidades[i][j]=5;
+               }
+
+               if((i == 3 && j == 7)||
+                  (i == 4 && j == 6)||
+                  (i == 4 && j == 7)||
+                  (i == 4 && j == 8)||
+                  (i == 5 && j == 5)||
+                  (i == 5 && j == 6)||
+                  (i == 5 && j == 7)||
+                  (i == 5 && j == 8)||
+                  (i == 5 && j == 9)||
+                  (i == 6 && j == 6)||
+                  (i == 6 && j == 7)||
+                  (i == 6 && j == 8)||
+                  (i == 7 && j == 7))
+                  {
+                    tabuleiroHabilidades[i][j] = 9;
+                  }
+
+                  if((i == 5 && j == 2)||
+                     (i == 6 && j == 1)||
+                     (i == 6 && j == 2)||
+                     (i == 6 && j == 3)||
+                     (i == 7 && j == 0)||
+                     (i == 7 && j == 1)||
+                     (i == 7 && j == 2)||
+                     (i == 7 && j == 3)||
+                     (i == 7 && j == 4))
+                     {
+                        tabuleiroHabilidades[i][j] = 4;
+                     }
+
+            printf("%d ",tabuleiroHabilidades[i][j]); 
+        }
+        printf("\n");
+    }
 
     return 0;
 }
